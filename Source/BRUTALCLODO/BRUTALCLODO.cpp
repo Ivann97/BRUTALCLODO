1647a5e2a479b343b14b1b7777378a4b51fd65c6
Ivann97 <ivann.schnell@telecomnancy.net>
2020-05-20 14:17:55 +0200

    Initial commit

---
+++ Source/BRUTALCLODO/BRUTALCLODO.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "BRUTALCLODO.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BRUTALCLODO, "BRUTALCLODO" );
+ 
---
+++ Source/BRUTALCLODO/BRUTALCLODOGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BRUTALCLODOGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABRUTALCLODOGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABRUTALCLODOGameMode();
+};
+
+
+
